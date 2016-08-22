c7d35036a44f488a448a1dff004446dd5ab7458f
krabrr <nbaramichai@gmail.com>
2016-08-22 21:42:34 +0700

    init

---
+++ word.js
@@ -0,0 +1,25 @@
+var dict, reader;
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  dict = new Object();
+  reader = new XMLHttpRequest();
+  loadDictionary();
+}
+
+function loadDictionary() {
+  reader.open("GET", "dict.txt");
+  reader.onreadystatechange = loadDictionaryCompleteHandler;
+  reader.send(null);
+}
+
+function loadDictionaryCompleteHandler() {
+  if (reader.readyState == 4) {
+    var line, lines
+    lines = reader.responseText.split(/[\n\r]/g);
+    lines.forEach(function(line) {
+      dict[line] = true;
+    })
+  }
+}
