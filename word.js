102aaea54c9d5e1bb4912a6158bffeb790e59f17
Nueng <nueng@thinkcubic.com>
2016-08-23 08:55:07 +0700

    update

--- word.js
+++ word.js
@@ -1,4 +1,6 @@
-var dict, reader;
+var dict, reader,
+  two = [], three = [], four = [],
+  five = [], six = [], seven = [];
 
 window.addEventListener("DOMContentLoaded", init);
 
@@ -19,7 +21,30 @@ function loadDictionaryCompleteHandler() {
     var line, lines
     lines = reader.responseText.split(/[\n\r]/g);
     lines.forEach(function(line) {
+      line = line.toLowerCase();
       dict[line] = true;
+      switch (line.length) {
+        case 2:
+          two.push(line);
+          break;
+        case 3:
+          three.push(line);
+          break;
+        case 4:
+          four.push(line);
+          break;
+        case 5:
+          five.push(line);
+          break;
+        case 6:
+          six.push(line);
+        case 7:
+          seven.push(line);
+      }
     })
   }
+}
+
+function solve(table, num_words) {
+
 }
