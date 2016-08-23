b11223af072fb5e5156e463a32ec773c7b4aca91
Nueng <nueng@thinkcubic.com>
2016-08-23 11:18:55 +0700

    update

--- word.js
+++ word.js
@@ -1,12 +1,14 @@
-var dict, reader, len_map, test = true,
+var dict, reader, len_map, pos_map, test = true,
   two = [], three = [], four = [],
   five = [], six = [], seven = [],
-  eigth = [], base = 'a'.charCodeAt(0);
+  eigth = [], base = 'a'.charCodeAt(0),
+  table, filterd_words;
 
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
   dict = new Object();
+  pos_map = new Object();
   len_map = new Object();
   len_map[2] = two;
   len_map[3] = three;
@@ -41,9 +43,7 @@ function loadDictionaryCompleteHandler() {
 
 function getWordCharArray(word) {
   var i, chn, char_arr = [];
-  for (i = 0; i < 26; i++) {
-    char_arr.push(0);
-  }
+  for (i = 0; i < 26; i++) char_arr.push(0);
   for (i = 0; i < word.length; i++) {
     chn = word.charCodeAt(i) - base;
     char_arr[chn]++;
@@ -51,30 +51,22 @@ function getWordCharArray(word) {
   return char_arr;
 }
 
-function isPossibleLevel1(main, sub) {
+function isPossible(main, sub) {
   var i;
   for (i = 0; i < 26; i++) {
-    if (main[i] < sub[i]) {
-      return false;
-    }
+    if (main[i] < sub[i]) return false;
   }
   return true;
 }
 
 function permutation(arr, used, result) {
   var i, sub;
-  if (!used) {
-    used = [];
-  }
-  if (!result) {
-    result = [];
-  }
+  if (!used) used = [];
+  if (!result) result = [];
   for (i = 0; i < arr.length; i++) {
     sub = arr.splice(i, 1)[0];
     used.push(sub);
-    if (!arr.length) {
-      result.push(used.concat());
-    }
+    if (!arr.length) result.push(used.concat());
     permutation(arr, used, result);
     arr.splice(i, 0, sub);
     used.pop();
@@ -82,38 +74,90 @@ function permutation(arr, used, result) {
   return result;
 }
 
+function subtract(main, sub) {
+  var i, result = [];
+  for (i = 0; i < 26; i++) {
+    if (main[i] < sub[i]) return null;
+    result[i] = main[i] - sub[i];
+  }
+  return result;
+}
+
+function getResult(order, info) {
+  var i, words, word, tmp_1, tmp_2,
+    new_info = new Object();
+  words = filterd_words[info.level];
+  for (i = 0; i < words.length; i++) {
+    word = words[i];
+    tmp_1 = subtract(info.char_arr, getWordCharArray(word));
+    if (!tmp_1) return;
+
+    // add more condition here
+
+    // base case
+    if (info.level == order.length - 1) {
+      info.result.push(info.word_set);
+    } else {
+      new_info.char_arr = tmp_1;
+      new_info.result = info.result;
+      new_info.level = info.level + 1;
+      new_info.word_set = info.word_set.concat([word]);
+      getResult(order, new_info);
+    }
+  }
+}
+
 function solve(table, num_words) {
-  var i, j, ch, chn, row, char_arr,
-    num_word, tmp_arr, result,
-    pos_words, filterd_pos_words;
+  var i, j, ch, chn, num_word,
+    char_arr, tmp_arr_1, tmp_arr_2,
+    perm_arr, pos_words, info, result;
   if (test) { // override actual data with test data
     table = [['e', 'i', 'd', 't'], ['r', 's', 'o', 'n'], ['g', 'i', 'a', 'l']];
     num_words = [5, 7]
   }
+
+  this.table = table;
+
   char_arr = [];
-  for (i = 0; i < 26; i++) {
-    char_arr.push(0);
-  }
+  for (i = 0; i < 26; i++) char_arr.push(0);
   for (i = 0; i < table.length; i++) {
+    pos_map[i] = new Object();
     for (j = 0; j < table[i].length; j++) {
       ch = table[i][j];
+      pos_map[i][j] = ch;
       chn = ch.charCodeAt(0) - base;
       char_arr[chn]++;
     }
   }
 
   // step 1: filter all possible word
-  result = [];
+  filterd_words = [];
   for (i = 0; i < num_words.length; i++) {
-    filterd_pos_words = [];
+    tmp_arr_1 = [];
     num_word = num_words[i];
     pos_words = len_map[num_word];
     for (j = 0; j < pos_words.length; j++) {
-      tmp_arr = getWordCharArray(pos_words[j]);
-      if (isPossibleLevel1(char_arr, tmp_arr)) {
-        filterd_pos_words.push(pos_words[j]);
+      tmp_arr_2 = getWordCharArray(pos_words[j]);
+      if (isPossible(char_arr, tmp_arr_2)) {
+        tmp_arr_1.push(pos_words[j]);
       }
     }
-    result.push(filterd_pos_words)
+    filterd_words.push(tmp_arr_1)
+  }
+
+  // step 2: filter with more restrict condition
+  result = [];
+  tmp_arr_1 = []
+  for (i = 0; i < num_words.length; i++) tmp_arr.push(i);
+  perm_arr = permutation(tmp_arr_1);
+  for (i = 0; i < perm_arr.length; i++) {
+    info = new Object();
+    info.level = 0;
+    info.result = [];
+    info.word_set = [];
+    info.table = table;
+    info.pos_map = pos_map;
+    info.char_arr = char_arr.concat();
+    result = result.concat(getResult(perm_arr[i], info));
   }
 }
